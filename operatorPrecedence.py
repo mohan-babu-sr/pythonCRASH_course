3c2b2a876ef720f3cffa20f75c508f8298f2610b
MOHAN BABU S R <83107890+mohan-drive@users.noreply.github.com>
2022-12-05 12:10:06 +0530

    Order of precedence

---
+++ operatorPrecedence.py
@@ -0,0 +1,16 @@
+# Order of precedence
+print('''# Order of precedence
+  0 -> Parenthesis
+  1 -> Exponentiation 2**3
+  2 -> multiplication or division
+  3 -> addition or subtraction
+''')
+
+x = 10 + 3 * 2
+print(x)
+
+y = 10 + 3 * 2 ** 2
+print(y)
+
+z = 10 + 3 * 2 ** 2
+print(z)
